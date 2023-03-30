578b5655759b1934286c179bcef78af185116852
Robert Wiggins <51722811+RandomRobbieBF@users.noreply.github.com>
2023-03-30 11:15:50 +0100

    Update chart-down.go

--- chart-down.go
+++ chart-down.go
@@ -32,55 +32,59 @@ func main() {
 		log.Fatal(err)
 	}
 	defer resp.Body.Close()
-
-	// create output file
-	f, err := os.Create("charts.txt")
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer f.Close()
-
-	// create a new YAML decoder
-	decoder := yaml.NewDecoder(resp.Body)
-
-	// loop through YAML documents in the stream
-	for {
-		var doc map[string]interface{}
-		err := decoder.Decode(&doc)
-		if err == io.EOF {
-			break
-		}
+	if resp.StatusCode == http.StatusOK {
+		// create output file
+		f, err := os.Create("charts.txt")
 		if err != nil {
 			log.Fatal(err)
 		}
+		defer f.Close()
 
-		// extract data from YAML document
-		entries := doc["entries"].(map[interface{}]interface{})
-		for _, entry := range entries {
-			charts := entry.([]interface{})
-			for _, chart := range charts {
-				chartData := chart.(map[interface{}]interface{})
-				name := chartData["name"].(string)
-				description := chartData["description"].(string)
-				urls := chartData["urls"].([]interface{})
-				version := chartData["version"].(string)
+		// create a new YAML decoder
+		decoder := yaml.NewDecoder(resp.Body)
 
-				// print out data
-				fmt.Printf("Name: %s\n", name)
-				fmt.Printf("Description: %s\n", description)
-				for _, u := range urls {
-					url := u.(string)
-					if !strings.HasPrefix(url, "http") {
-						url = "" + urlz + "/" + url + ""
+		// loop through YAML documents in the stream
+		for {
+			var doc map[string]interface{}
+			err := decoder.Decode(&doc)
+			if err == io.EOF {
+				break
+			}
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			// extract data from YAML document
+			entries := doc["entries"].(map[interface{}]interface{})
+			for _, entry := range entries {
+				charts := entry.([]interface{})
+				for _, chart := range charts {
+					chartData := chart.(map[interface{}]interface{})
+					name := chartData["name"].(string)
+					description := chartData["description"].(string)
+					urls := chartData["urls"].([]interface{})
+					version := chartData["version"].(string)
+
+					// print out data
+					fmt.Printf("Name: %s\n", name)
+					fmt.Printf("Description: %s\n", description)
+					for _, u := range urls {
+						url := u.(string)
+						if !strings.HasPrefix(url, "http") {
+							url = "" + urlz + "/" + url + ""
+						}
+						fmt.Printf("URL: %s\n", url)
+						_, err := f.WriteString(url + "\n")
+						if err != nil {
+							log.Fatal(err)
+						}
 					}
-					fmt.Printf("URL: %s\n", url)
-					_, err := f.WriteString(url + "\n")
-					if err != nil {
-						log.Fatal(err)
-					}
+					fmt.Printf("Version: %s\n\n", version)
 				}
-				fmt.Printf("Version: %s\n\n", version)
 			}
 		}
+	} else {
+		fmt.Printf("None HTTP 200 Resoponse Returned")
 	}
+
 }
